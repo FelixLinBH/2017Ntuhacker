58ca000e628284b46a1bff871dd9f252a6f5c347
Felix.lin <fly_81211@hotmail.com>
2017-07-21 21:13:17 +0800

    test

--- credit.py
+++ credit.py
@@ -10,6 +10,15 @@ class Card:
 		self.AvailableCredit = AvailableCredit
 		self.CreditCardLimit = CreditCardLimit
 
+class CreidtReposed:
+	def __init__(self,AcctNbr,StatementDate,TransactionAmt,TransactionDate,TransactionDescChinese,TransactionPostingDate):
+		self.AcctNbr = AcctNbr
+		self.StatementDate = StatementDate
+		self.TransactionAmt = TransactionAmt
+		self.TransactionDate = TransactionDate
+		self.TransactionDescChinese = TransactionDescChinese
+		self.TransactionPostingDate = TransactionPostingDate
+
 class Account:
 	serverUrl = "http://54.65.120.143:8888/hackathon/"
 	token = ""
@@ -28,6 +37,7 @@ class Account:
 			self.token = auth['Token']
 			self.getCreditCard()
 
+
 	def getCreditCard(self):
 		url = self.serverUrl + "CreditCardLimit"
 		payload = {'CustID': self.CustID, 'Token': self.token }
@@ -38,6 +48,7 @@ class Account:
 				card = Card(item['CardNO'],item['CreditInterestRate'],item['AvailableCredit'],item['CreditCardLimit'])
 				self.cardArray.append(card)
 	
+
 	def buySomething(self,cardNo,tranAmt,Note):
 		self.login()
 		url = self.serverUrl + "CreditCardAuthorize"
@@ -46,19 +57,28 @@ class Account:
 		if r.status_code == 200:
 			data = json.loads(r.text)
 			if data['TranAmt'] == tranAmt:
+				data['note'] = Note
+				self.writeNewBuyItem(data)
 				return 1
 		return 0
 
 	# fake list
 	def getBuyList(self):
-		self.login()
-		url = self.serverUrl + "PaymentsDue"
-		payload = {'CustID': self.CustID, 'Token': self.token }
-		print(payload)
-		r = requests.post(url, json=payload)
-		if r.status_code == 200:
-			data = json.loads(r.text)
-			print(data)
+		with open('list2.json') as json_data:
+			d = json.load(json_data)
+		return d
+
+	def writeBuyList(self,data):
+		with open('list2.json', 'w') as outfile:
+			json.dump(data, outfile)
+
+
+   	def writeNewBuyItem(self,item):
+   		oldList = self.getBuyList()
+		oldList.append(item)
+		print(oldList)
+		self.writeBuyList(oldList)
+
 			
 		
 
@@ -71,13 +91,13 @@ class Account:
 
 account = Account("B199443055","3055")
 account.login()
-print(account.cardArray[0].AvailableCredit)
-# account.getBuyList()
+# print(account.cardArray[0].AvailableCredit)
+# account.writeNewBuyItem()
 # account.login()
 # account.getCreditCard()
-
-# print(account.buySomething(account.cardArray[0].CardNO,'100','test'))
-
+# account.buySomething(account.cardArray[0].CardNO,'100.00','Under Armour')
+t = account.getBuyList()
+print(t)
 
 
 
